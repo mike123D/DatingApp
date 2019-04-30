19ad988bb43ba09a4760af3b2d6ffdc422404fc5
Sena <semahdiya@gmail.com>
2019-04-30 19:58:04 +0000

    End of section 5

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -49,6 +49,9 @@ namespace DatingApp.API.Controllers
         [HttpPost("login")]
         public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
         {
+           
+
+           
             var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
             if (userFromRepo == null)
                 return Unauthorized();
@@ -73,6 +76,9 @@ namespace DatingApp.API.Controllers
             return Ok(new {
                 token =tokenHandler.WriteToken(token)
             });
+
+            
+            
         }
 
     }
